4a20dde6d9d80e4f68858d9fa112f18f2c59129b
Sridhar Bommena <smartboysridhar@gmail.com>
2020-10-03 17:51:24 +0530

    Successful InstaDp app for android.
    future we can expect some updates for this app

---
+++ App.js
@@ -0,0 +1,213 @@
+import React, { useState } from 'react';
+import { ActivityIndicator, TouchableOpacity ,PermissionsAndroid } from 'react-native';
+import { StyleSheet, Text, TextInput, View , Dimensions, KeyboardAvoidingView, ScrollView, StatusBar  } from 'react-native';
+import { Image, Input } from 'react-native-elements';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { AntDesign } from '@expo/vector-icons';
+import * as MediaLibrary from 'expo-media-library';
+import * as FileSystem from 'expo-file-system';
+import * as Permissions from 'expo-permissions';
+import { Overlay } from 'react-native-elements';
+
+
+export default function App() {
+  const [link , setLink] = useState("");
+  const [bio , setBio ] = useState("");
+  const [dpURL , setDpURL] = useState("");
+  const [fullName , setFullName ] = useState("");
+  const [loading , setLoading ] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  const toggleOverlay = () => {
+    setVisible(!visible);
+  };
+
+  const downloadFile= () =>{
+
+    toggleOverlay();
+
+    const uri = dpURL;
+    var filename = "";
+    const index = link.lastIndexOf("com/");
+    const SecondIndex = link.lastIndexOf("?");
+      filename = link.slice(index+4 , SecondIndex);
+
+    let fileUri = FileSystem.documentDirectory + filename +".jpg";
+    FileSystem.downloadAsync(uri, fileUri)
+    .then(({ uri }) => {
+        saveFile(uri);
+      })
+      .catch(error => {
+        console.error(error);
+      })
+}
+
+const saveFile = async (fileUri) => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status === "granted") {
+        const asset = await MediaLibrary.createAssetAsync(fileUri)
+        await MediaLibrary.createAlbumAsync("Download", asset, false);
+        alert("SUCESS : file downloaded successfully");
+        setVisible(false);
+    }
+}
+
+
+  const getData = async(value) =>
+{
+  setLoading(true);
+  
+  if(value)
+  {
+    
+    var newValue = "";
+    const index = value.lastIndexOf("?");
+    if(index != -1)
+    {
+      newValue = value.slice(0,index);
+      console.log(newValue);
+    }
+    else
+    {
+      newValue = value;
+    }
+    const url = newValue.concat("?__a=1");
+    console.log(url);
+    await fetch(url)
+    .then(response => response.json())
+    .then( json => {
+      setBio(json.graphql.user.biography);
+      setFullName(json.graphql.user.full_name);
+      setDpURL(json.graphql.user.profile_pic_url_hd);
+      // console.log(json.graphql.user.biography);
+      // console.log(json.graphql.user.full_name);
+      // console.log(json.graphql.user.profile_pic_url_hd);
+    } )
+    .catch(error => console.log(error));
+  }
+  else
+  {
+    alert("Error : No URL entered or Invalid URL ");
+    setBio("");
+    setDpURL("");
+    setFullName("");
+  }
+  setLoading(false);
+
+}
+
+  return (
+    <View style={styles.container}>
+      <SafeAreaView style={styles.view} >
+        <ScrollView>
+          <Text style={styles.text} >Insta Downloader</Text>
+        <View style={styles.InputContainer}>
+        <TextInput style={styles.textInput} value={link} onChangeText={text => setLink(text)}  />
+        <TouchableOpacity style={{
+          backgroundColor : "skyblue" ,
+          padding : height*0.025 ,
+          borderRadius : height /10,
+          }} onPress={() =>{ getData(link);} }   
+        >
+          {
+            loading ?
+            <ActivityIndicator
+              color="black"
+          />            
+            :
+        <AntDesign name="arrowright" size={24} color="black" />
+          }
+        
+        </TouchableOpacity>
+        </View>
+        
+        <View style={styles.downContainer}>
+        <Overlay isVisible={visible} >
+        <ActivityIndicator
+              color="black"
+              size="large"
+          /> 
+          <Text style={{fontSize:20 , fontWeight : "bold"}} >Downloading..</Text>
+      </Overlay>
+        <View style={{ padding: 15 }} >
+            <Text style={{fontSize: 25 , fontWeight : "bold" }}>{fullName}</Text>
+          </View>
+          <View>
+            <Text style={{fontSize: 18}} >{bio}</Text>
+          </View>
+        </View>
+          
+        <View style={styles.ImageContainer} > 
+        <TouchableOpacity onPress={()=>downloadFile()} >
+        <Image
+        source={ dpURL? { uri: dpURL }: { uri:"https://www.bestofelectricals.com/images/default-image.png" }}
+        style={{ width: 350, height: 350 }}
+        resizeMode = "contain"
+        PlaceholderContent={<ActivityIndicator />}
+          />
+        </TouchableOpacity>
+         
+          <Text style={styles.text} >Insta Downloader</Text>
+
+        </View>
+
+
+        </ScrollView>
+      </SafeAreaView>
+    </View>
+  );
+}
+
+const height = Dimensions.get("screen").height;
+const width = Dimensions.get("screen").width ;
+const statusbarHeight = StatusBar.currentHeight;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  textInput : 
+  {
+    width : width * 0.7 ,
+    paddingVertical : height * 0.02 ,
+    borderWidth : 1,
+    fontSize  : height * 0.025,
+    borderRadius : 30,
+    paddingLeft : width * 0.07,
+    paddingRight :width * 0.07,
+  },
+  view : 
+  {
+    flex : 1 ,
+    marginTop : statusbarHeight * 2 ,
+  },
+  ImageContainer : 
+  {
+    top : height * 0.08,
+    justifyContent : "center",
+    alignItems : "center",
+  },
+  InputContainer:
+  {
+    flex : 1,
+    alignItems : "center",
+    flexDirection : "row",
+    justifyContent : "space-between",
+    marginHorizontal : width * 0.05,
+  },
+  text :
+  {
+    fontSize : 40 ,
+    fontWeight : "bold",
+    color : "red",
+    alignSelf : "center",
+    marginBottom : statusbarHeight * 0.5
+  },
+  downContainer : 
+  {
+    top : height * 0.05,
+    justifyContent : "center",
+    alignItems : "center",
+  }
+});
---
+++ README.md
@@ -0,0 +1,12 @@
+**React Native App for Instagram Dp download**
+instadp
+it is a react native project
+
+usage :
+
+open any instagram user profile
+copy the profile url.
+paste it in the textbox in this app
+click next
+click on the image
+the image will be downloaded to your device.
